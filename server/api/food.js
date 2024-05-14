377081f450063cf95c6671f483be554a5053090a
Marisa <marisavandellos@gmail.com>
2024-05-14 16:30:07 -0700

    made skeleton for api routers

--- server/api/food.js
+++ server/api/food.js
@@ -1,1 +1,23 @@
 //meals and food
+const express = require('express');
+const foodRouter = express.Router();
+
+const { requireUser } = require('../auth/utils');
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+//<--------------------------GET ALL MEALS------------------------>
+//<--------------------------GET SINGLE MEAL------------------------>
+//<--------------------------POST NEW MEAL------------------------>
+//<--------------------------DELETE MEAL------------------------>
+//<--------------------------PATCH MEAL------------------------>
+//<--------------------------GET ALL FOOD------------------------>
+//<--------------------------POST NEW FOOD------------------------>
+//<--------------------------DELETE FOOD------------------------>
+//<--------------------------PATCH FOOD------------------------>
+//<--------------------------FOOD PURCHASE TOGGLE------------------------>
+//<--------------------------FOOD COOLER TOGGLE------------------------>
+
+
+module.exports = foodRouter;
--- server/auth/index.js
+++ server/auth/index.js
@@ -1,1 +1,34 @@
 //user, equipment, and campgrounds
+const express = require("express");
+const authRouter = express.Router();
+
+const {requireAdmin, requireUser} = require("./utils");
+
+const {PrismaClient} = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const jwt = require("jsonwebtoken");
+const {JWT_SECRET} = process.env;
+const bcrypt = require("bcrypt");
+const SALT_COUNT = 10;
+
+//<--------------------------GET ALL USERS------------------------>
+//<--------------------------GET SINGLE USER------------------------>
+//<--------------------------LOGIN------------------------>
+//<--------------------------DELETE USER------------------------>
+//<--------------------------PATCH USER------------------------>
+//<--------------------------GET ALL CAMPGROUNDS------------------------>
+//<--------------------------GET SINGLE CAMPGROUND------------------------>
+//<--------------------------POST CAMPGROUND------------------------>
+//<--------------------------DELETE CAMPGROUND------------------------>
+//<--------------------------PATCH CAMPGROUND------------------------>
+//<--------------------------GET ALL EQUIPMENT------------------------>
+//<--------------------------POST EQUIPMENT------------------------>
+//<--------------------------DELETE EQUIPMENT------------------------>
+//<--------------------------PATCH EQUIPMENT------------------------>
+//<--------------------------EQUIPMENT PACK TOGGLE------------------------>
+//<--------------------------EQUIPMENT NEED TOGGLE------------------------>
+//<--------------------------DELETE TRIP------------------------>
+
+
+module.exports = authRouter;
