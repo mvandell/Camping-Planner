5d31d29841c4949256bb09992b08bd1e00cb0a5e
mvandell <136928205+mvandell@users.noreply.github.com>
2024-05-20 09:56:59 -0700

    GET routes for food done

--- server/api/food.js
+++ server/api/food.js
@@ -8,13 +8,44 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 //<--------------------------GET ALL MEALS------------------------>
+//GET api/food/meal
+foodRouter.get("/meal", requireUser, async (req, res, next) => {
+    try {
+        const allMeals = await prisma.meals.findMany();
+        res.send(allMeals);
+    } catch (error) {
+        next(error);
+    }
+});
 //<--------------------------GET SINGLE MEAL------------------------>
-//<--------------------------POST NEW MEAL------------------------>
-//<--------------------------DELETE MEAL------------------------>
-//<--------------------------PATCH MEAL------------------------>
+//GET api/food/meal/:id
+foodRouter.get("/meal/:id", requireUser, async (req, res, next) => {
+    try {
+        const meal = await prisma.meals.findUnique({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.send(meal);
+    } catch (error) {
+        next(error);
+    }
+});
 //<--------------------------GET ALL FOOD------------------------>
-//<--------------------------POST NEW FOOD------------------------>
+//GET api/food/food
+foodRouter.get("/food", requireUser, async (req, res, next) => {
+    try {
+        const allFood = await prisma.meals.findMany();
+        res.send(allFood);
+    } catch (error) {
+        next(error);
+    }
+});
+//<--------------------------DELETE MEAL------------------------>
 //<--------------------------DELETE FOOD------------------------>
+//<--------------------------POST NEW MEAL------------------------>
+//<--------------------------POST NEW FOOD------------------------>
+//<--------------------------PATCH MEAL------------------------>
 //<--------------------------PATCH FOOD------------------------>
 //<--------------------------FOOD PURCHASE TOGGLE------------------------>
 //<--------------------------FOOD COOLER TOGGLE------------------------>
