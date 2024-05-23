073d4cd840b436d0d75efcebba7fb131ede5d9ad
Marisa <marisavandellos@gmail.com>
2024-05-23 14:18:41 -0700

    started app.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const App = () => {
-  return <h1>React is awesome</h1>;
+  const token = useSelector((state) => state.auth.token);
+  console.log(token) //delete when website finished
+
+  return (
+    <div className="App">
+      <Routes>
+
+      </Routes>
+    </div>
+  )
 };
 
 export default App;
