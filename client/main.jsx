f6728a246fb131aaed5ecf95b8579515c84155f5
Marisa <marisavandellos@gmail.com>
2024-05-23 14:12:28 -0700

    made main.jsx

--- client/main.jsx
+++ client/main.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store"
 
 const root = createRoot(document.getElementById("app"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
